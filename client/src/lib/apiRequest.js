7ab602cd8b3850aecf392adc560983313baa7d29
anes00a <168821849+anes00a@users.noreply.github.com>
2024-09-09 22:50:05 +0100

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://project-real-estate-backend-api.onrender.com",
   withCredentials: true,
 });
 
